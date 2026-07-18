2b273c6bdff7aac6ce74d10e23acb355018248ee
haseebfarooqi101 <haseeb.farooqi@intelytics.co>
2026-07-18 10:00:39 +0500

    Update sitemap.ts

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -3,7 +3,7 @@ import { MetadataRoute } from 'next';
 export default function sitemap(): MetadataRoute.Sitemap {
   return [
     {
-      url: 'https://vercel.app',
+      url: 'https://portfolio-muhammad-haseeb-farooqi-dev.vercel.app',
       lastModified: new Date(),
       changeFrequency: 'monthly',
       priority: 1,
